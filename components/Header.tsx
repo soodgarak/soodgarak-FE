310b286f9f06a3b4f7cde393782d1fde0b91a6fd
hyunsunglimm <gustjd3834@naver.com>
2024-07-24 21:13:47 +0900

    Fix: header 로고 조정
    width 조정 및 홈 이동

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import SearchIcon from '@/components/icon/SearchIcon';
 import { useRef } from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -13,14 +14,16 @@ const Header = () => {
 
   return (
     <header className='sticky top-0 flex gap-8 bg-white p-8'>
-      <Image
-        src='/images/soodgarak-logo.png'
-        alt='soodgarak logo'
-        width={120}
-        height={48}
-        className='h-[4.8rem] w-[12rem]'
-      />
-      <div className='relative w-full'>
+      <Link href='/'>
+        <Image
+          src='/images/soodgarak-logo.png'
+          alt='soodgarak logo'
+          width={120}
+          height={48}
+          className='h-[4.8rem] w-[12rem]'
+        />
+      </Link>
+      <div className='relative grow'>
         <input
           className='bg-gray-sm h-[4.8rem] w-full rounded-md pl-[4.8rem] text-[1.6rem]'
           placeholder='요리 or 재료'
