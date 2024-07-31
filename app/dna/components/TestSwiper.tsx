64f35bcf67ba6889faa0be0b06ed1b7981700ba3
hyunsunglimm <gustjd3834@naver.com>
2024-07-31 14:12:34 +0900

    Feat: 카드 스와이프 프로토타입 1차

--- app/dna/components/TestSwiper.tsx
+++ app/dna/components/TestSwiper.tsx
@@ -4,14 +4,74 @@ import { DUMMY_FOODS } from '@/mock/data';
 import Image from 'next/image';
 import HateButton from './HateButton';
 import LikeButton from './LikeButton';
+import { useEffect, useRef, useState } from 'react';
 
 const TestSwiper = () => {
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+  const [moveX, setMoveX] = useState(0);
+  const [moveY, setMoveY] = useState(0);
+
+  const currentCard = useRef<HTMLDivElement | null>(null);
+  const frame = useRef<HTMLDivElement | null>(null);
+
+  const setTransform = (x: number, y: number, deg: number, duration?: number | undefined) => {
+    if (currentCard.current) {
+      currentCard.current.style.transform = `translate3d(${x}px, ${y}px, 0) rotate(${deg}deg)`;
+      if (duration) currentCard.current.style.transition = `transform ${duration}ms`;
+    }
+  };
+
+  const complete = () => {};
+
+  const cancel = () => {
+    setTransform(0, 0, 0, 100);
+    setTimeout(() => {
+      if (currentCard.current) currentCard.current.style.transition = '';
+    }, 100);
+  };
+
+  const onPointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
+    const x = e.clientX - startX;
+    const y = e.clientY - startY;
+    setMoveX(x);
+    setMoveY(y);
+
+    setTransform(x, y, (x / window.innerWidth) * 50);
+  };
+
+  const onPointerUp = (e: React.PointerEvent<HTMLDivElement>) => {
+    if (!frame.current || !currentCard.current) return;
+    currentCard.current.removeEventListener('pointermove', onPointerMove as any);
+    currentCard.current.removeEventListener('pointerup', onPointerUp as any);
+    currentCard.current.removeEventListener('pointerleave', onPointerUp as any);
+    if (Math.abs(moveX) > frame.current.clientWidth / 2) {
+      currentCard.current.removeEventListener('pointerdown', onPointerDown as any);
+      complete();
+    } else {
+      cancel();
+    }
+  };
+
+  const onPointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+    setStartX(e.clientX);
+    setStartY(e.clientY);
+
+    if (currentCard.current) {
+      currentCard.current.addEventListener('pointermove', onPointerMove as any);
+      currentCard.current.addEventListener('pointerup', onPointerUp as any);
+      currentCard.current.addEventListener('pointerleave', onPointerUp as any);
+    }
+  };
+
   return (
-    <section className='relative flex grow flex-col items-center'>
-      {DUMMY_FOODS.map((food) => (
+    <section className='relative flex grow flex-col items-center' ref={frame}>
+      {DUMMY_FOODS.map((food, index) => (
         <div
           key={food.name}
           className='absolute h-5/6 w-[50rem] cursor-grab touch-none overflow-hidden rounded-16 bg-white'
+          onPointerDown={onPointerDown}
+          ref={index === DUMMY_FOODS.length - 1 ? currentCard : null}
         >
           <Image
             src={food.mainImg}
