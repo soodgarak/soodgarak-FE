95200d8196f7ec138133481762ca3a90b7613287
hyunsunglimm <gustjd3834@naver.com>
2024-08-01 14:05:26 +0900

    Refactor: 카드 스와이프 관련 컴포넌트 정리

--- app/dna/page.tsx
+++ app/dna/page.tsx
@@ -1,9 +1,7 @@
 import CardSwiper from './components/CardSwiper';
-import TestSwiper from './components/TestSwiper';
-import TestSwiper2 from './components/TestSwiper2';
 
 const DNAPage = () => {
-  return <TestSwiper2 />;
+  return <CardSwiper />;
 };
 
 export default DNAPage;
