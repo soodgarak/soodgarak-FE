a4d8aaa1fcf5a93875736c95c590cdabd6dc84ae
hyunsunglimm <gustjd3834@naver.com>
2024-08-22 09:33:41 +0900

    fix: 로딩중에 cursor-not-allowed 적용

--- app/dna/components/FoodDnaLanding.tsx
+++ app/dna/components/FoodDnaLanding.tsx
@@ -29,7 +29,7 @@ const FoodDnaLanding = () => {
       <h1 className='text-center text-28'>
         푸드 DNA 검사를 완료하면 사용자의 취향을 기반으로 음식을 추천받을 수 있습니다.
       </h1>
-      <Button onClick={goToFoodDnaTest}>
+      <Button onClick={goToFoodDnaTest} className={isLoading ? 'cursor-not-allowed' : ''}>
         {isLoading ? '음식 불러오는중...' : '검사하러 가기'}
       </Button>
     </section>
--- components/Button.tsx
+++ components/Button.tsx
@@ -3,13 +3,14 @@ import Spinner from './Spinner';
 type ButtonProps = {
   isLoading?: boolean;
   width?: string;
+  className?: string;
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ isLoading = false, width, children, ...props }: ButtonProps) => {
+const Button = ({ isLoading = false, width, children, className, ...props }: ButtonProps) => {
   return (
     <button
-      className={`bg-primary h-[6rem] rounded-12 p-12 text-24 font-bold text-white ${width || ''}`}
+      className={`h-[6rem] rounded-12 bg-primary p-12 text-24 font-bold text-white ${width || ''} ${isLoading && 'cursor-not-allowed'} ${className}`}
       {...props}
     >
       {isLoading ? <Spinner /> : children}
