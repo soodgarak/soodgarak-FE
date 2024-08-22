4a8d546222c1742e6b2ed995833f86616529734c
hyunsunglimm <gustjd3834@naver.com>
2024-08-22 16:19:26 +0900

    fix: dna페이지에 접근할 때 마다 새로운 요리 패칭

--- app/dna/components/FoodDnaLanding.tsx
+++ app/dna/components/FoodDnaLanding.tsx
@@ -14,7 +14,8 @@ const FoodDnaLanding = () => {
     const queryClient = getQueryClient();
     await queryClient.prefetchQuery({
       queryKey: ['dna'],
-      queryFn: async () => fetch('/api/dna').then((res) => res.json())
+      queryFn: async () => fetch('/api/dna').then((res) => res.json()),
+      staleTime: 0
     });
     setIsAgree(true);
     setIsLoading(false);
@@ -30,7 +31,7 @@ const FoodDnaLanding = () => {
         푸드 DNA 검사를 완료하면 사용자의 취향을 기반으로 음식을 추천받을 수 있습니다.
       </h1>
       <Button onClick={goToFoodDnaTest} disabled={isLoading}>
-        <p className='text-32 font-bold'>{isLoading ? '음식 불러오는중...' : '검사하러 가기'}</p>
+        <p className='text-32 font-bold'>{isLoading ? '음식 불러오는중...' : '검사하기'}</p>
       </Button>
     </section>
   );
