f3f2248e3ff171a7a0a312cc1c46b19ec2a83f32
jiyoon29 <yoon__29@naver.com>
2024-08-23 15:46:44 +0900

    style: tailwind.config 에 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,9 @@ const config: Config = {
       },
       colors: {
         'gray-sm': '#F1F1F1',
+        'gray-md': '#cccccc',
         primary: '#EBB22F',
+        tan: '#d2b48c',
         action: 'rgba(139, 69, 19, 0.2)'
       },
       fontSize: {
@@ -34,6 +36,7 @@ const config: Config = {
       },
       borderRadius: {
         none: '0',
+        4: '0.4rem',
         8: '0.8rem',
         12: '1.2rem',
         16: '1.6rem',
