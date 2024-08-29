2235e20434560b0acb74d874997ef573b9e5e564
hyunsunglimm <gustjd3834@naver.com>
2024-08-29 10:28:43 +0900

    feat: 검색 API 연결

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import FoodList from '@/components/FoodList';
 import { DUMMY_FOODS } from '@/mock/data';
 import { redirect } from 'next/navigation';
+import SearchResult from './components/SearchResult';
 
 type SearchPageProps = {
   searchParams: {
@@ -15,8 +16,7 @@ const SearchPage = ({ searchParams }: SearchPageProps) => {
   return (
     <section>
       <h1 className='mb-8 text-28 font-bold'>{`"${keyword}"의 검색결과`}</h1>
-      <p className='mb-16 text-end text-gray-500'>{DUMMY_FOODS.length}개의 요리들이 있습니다.</p>
-      <FoodList foods={DUMMY_FOODS} />
+      <SearchResult keyword={keyword} />
     </section>
   );
 };
--- service/food.ts
+++ service/food.ts
@@ -13,3 +13,10 @@ export const getFoodsByCategory = async (category: string): Promise<SimpleFood[]
 
   return foods;
 };
+
+export const getFoodsByKeyword = async (keyword: string): Promise<SimpleFood[]> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/recipe?keyword=${keyword}`);
+  const foods = await res.json();
+
+  return foods;
+};
